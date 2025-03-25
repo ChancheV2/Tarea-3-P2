a5556a9ad32f0ca4dc31544b19a7c20d2efd122f
Gabriel Sánchez <gabrielsr2604@gmail.com>
2025-03-24 23:15:38 -0400

    Creando Web

---
+++ Tarea 3/Tarea3-P2/ControlDeEvaluacionesEscolares.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ControlDeEvaluacionesEscolares.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
